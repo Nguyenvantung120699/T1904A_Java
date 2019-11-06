25bb834fd88cc922a1425bfd4d14098fb34e0c12
Nguyễn Văn Tùng <49889365+Nguyenvantung120699@users.noreply.github.com>
2019-11-06 14:41:57 +0700

    assignment3

---
+++ assignment3/Main.java
@@ -0,0 +1,30 @@
+package assignment3;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    public static Stage mainStage;
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        mainStage = primaryStage;
+        try{
+            Parent root = FXMLLoader.load(getClass().getResource("runtime.fxml"));
+            primaryStage.setTitle("Time countdown");
+            primaryStage.setScene(new Scene(root, 600, 400));
+            primaryStage.show();
+            
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
+}
